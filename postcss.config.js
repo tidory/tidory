1030ba9b6ff4d37307593889327fcab7982cea3a
pronist <pronist@naver.com>
2022-11-30 22:24:23 +0900

    Bump `tidory` from 8.2.4 to 8.3.0

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: [
-    require('postcss-import'),
+    // require('postcss-import'),
     require('tailwindcss/nesting'),
     require('tailwindcss'),
     require('autoprefixer')
